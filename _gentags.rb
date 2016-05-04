466e7451aac5a4ae0aeb45cf899109ec5bcdeb3f
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2016-05-04 10:18:43 +0200

    Autodetect tags

--- _gentags.rb
+++ _gentags.rb
@@ -1,4 +1,15 @@
-File.read("_taglist.txt").split("\n").each do |tag|
+require 'yaml'
+tags = []
+Dir.glob('_posts/*.md').each do |file|
+	begin
+		yaml_s = File.read(file).split(/^---$/)[1]
+		yaml_h = YAML.load(yaml_s)
+		tags += yaml_h['tags']
+	rescue
+	end
+end
+
+tags.uniq.each do |tag|
 	File.write "tags/#{tag}.html", <<-EOF
 ---
 layout: tagpage
